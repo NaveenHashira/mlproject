389546ee62dfa1e16d35951fb0cfd1fbace5880f
NaveenHashira <naveenmathaiyan12@gmail.com>
2025-03-25 17:02:10 +0530

    Logging and exception

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path,'r') as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name='mlproject',
+    version='0.1',
+    author='Naveen',
+    author_email='naveenmathaiyan12@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
